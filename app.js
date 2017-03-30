b1460d6025a6f26e2278ecaeeb96fa682ce1b10a
tiggleric <rhchen@andrew.cmu.edu>
2017-03-30 00:49:00 -0400

    Getting weather by zipcode to object

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 //Modules
-var weatherApp = angular.module('weatherApp',['ngRoute', 'ngResource']);
+var weatherApp = angular.module('weatherApp',['ngRoute', 'ngResource']).config(function($sceDelegateProvider) {
+     $sceDelegateProvider.resourceUrlWhitelist([
+       // Allow same origin resource loads.
+       'self',
+       // Allow loading from api.openweathermap.org
+       'http://api.openweathermap.org/**'
+     ])});
 
 //Routes
 weatherApp.config(function ($routeProvider){
@@ -16,11 +22,35 @@ weatherApp.config(function ($routeProvider){
     })
 });
 
-//Controllers
-weatherApp.controller('homeController', ['$scope', function($scope){
+//Services
+weatherApp.service('locationService', function() {
+    this.zipcode = "20817"
+    this.countryCode = "us" //USA, should be a constant
+})
+
+weatherApp.service('weatherService', function(){
     
+})
+
+//Controllers
+weatherApp.controller('homeController', ['$scope', 'locationService', function($scope, locationService){
+    
+    $scope.zipcode = locationService.zipcode;
+    $scope.searchKey = locationService.zipcode + "," + locationService.countryCode;
+    $scope.$watch('zipcode', function(){
+        locationService.zipcode = $scope.zipcode;
+        $scope.searchKey =  $scope.zipcode + "," +             locationService.countryCode;
+    });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope){
+weatherApp.controller('forecastController', ['$scope', '$resource', '$sce','locationService', function($scope, $resource, $sce, locationService){
     
+    $scope.searchKey = locationService.zipcode + "," + locationService.countryCode;
+    //?APPID=a4f92f0fd3e783c547e1af5127564493
+   
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=a4f92f0fd3e783c547e1af5127564493", {get : { method: "JSONP" }});
+    $scope.weatherResult = $scope.weatherAPI.get({ zip: $scope.searchKey });
+    
+    console.log("Search key: "+$scope.searchKey);
+    console.log($scope.weatherResult);
 }]);
