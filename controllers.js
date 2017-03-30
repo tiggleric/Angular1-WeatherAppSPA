0824c70829c92edb922a0971d12b129c40fef14b
Robert Chen <robert.chen@capitalone.com>
2017-03-30 19:54:31 -0400

    separated app.js to separate .js files

---
+++ controllers.js
@@ -0,0 +1,26 @@
+//Controllers
+weatherApp.controller('homeController', ['$scope', 'locationService', function($scope, locationService){
+    
+    $scope.zipcode = locationService.zipcode;
+    $scope.searchKey = locationService.zipcode + "," + locationService.countryCode;
+    $scope.$watch('zipcode', function(){
+        locationService.zipcode = $scope.zipcode;
+        $scope.searchKey =  $scope.zipcode + "," +             locationService.countryCode;
+    });
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$sce', 'locationService', function($scope, $resource, $sce, locationService){
+    
+    $scope.searchKey = locationService.zipcode + "," + locationService.countryCode;
+   
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=a4f92f0fd3e783c547e1af5127564493", {get : { method: "JSONP" }});
+    $scope.weatherResult = $scope.weatherAPI.get({ zip: $scope.searchKey });
+    
+    $scope.convertToFahrenheit = function(kelvin){
+        return Math.round((1.8 * (kelvin -273)) +32 );
+    }
+    
+    $scope.convertToDate = function(sec){
+        return new Date(sec * 1000);
+    }
+}]);
---
+++ services.js
@@ -0,0 +1,9 @@
+//Services
+weatherApp.service('locationService', function() {
+    this.zipcode = "20817"
+    this.countryCode = "us" //USA, should be a constant
+})
+
+weatherApp.service('weatherService', function(){
+    
+})
